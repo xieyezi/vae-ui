ebef290807cbe382912ab1b90712c0270938a5c2
xieyezi <suyechun@iwubida.com>
2020-10-10 16:22:02 +0800

    pref:update config

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,37 @@
-// 请先安装 rollup-plugin-esbuild rollup-plugin-vue rollup-plugin-scss sass rollup-plugin-terser
-// 为了保证版本一致，请复制我的 package.json 到你的项目，并把 name 改成你的库名
 import esbuild from 'rollup-plugin-esbuild'
 import vue from 'rollup-plugin-vue'
 import scss from 'rollup-plugin-scss'
-import dartSass from 'sass';
-import { terser } from "rollup-plugin-terser"
+import dartSass from 'sass'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'src/lib/index.ts',
-  output: [{
-    globals: {
-      vue: 'Vue'
+  output: [
+    {
+      globals: {
+        vue: 'Vue',
+      },
+      name: 'Vae',
+      file: 'dist/lib/vae.js',
+      format: 'umd',
+      plugins: [terser()],
     },
-    name: 'Vae',
-    file: 'dist/lib/vae.js',
-    format: 'umd',
-    plugins: [terser()]
-  }, {
-    name: 'Vae',
-    file: 'dist/lib/vae.esm.js',
-    format: 'es',
-    plugins: [terser()]
-  }],
+    {
+      name: 'Vae',
+      file: 'dist/lib/vae.esm.js',
+      format: 'es',
+      plugins: [terser()],
+    },
+  ],
   plugins: [
     scss({ include: /\.scss$/, sass: dartSass }),
     esbuild({
       include: /\.[jt]s$/,
       minify: process.env.NODE_ENV === 'production',
-      target: 'es2015' 
+      target: 'es2015',
     }),
     vue({
       include: /\.vue$/,
-    })
+    }),
   ],
 }
