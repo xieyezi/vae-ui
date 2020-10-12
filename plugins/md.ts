f74183cadeecc889f8201636cfa210118e307447
xieyezi <suyechun@iwubida.com>
2020-10-12 09:44:50 +0800

    docs:update md.ts

--- plugins/md.ts
+++ plugins/md.ts
@@ -4,18 +4,6 @@ import fs from 'fs'
 import marked from 'marked'
 
 
-
-// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-// 请注意，当前文件的后缀从 .js 改为了 .ts
-// 如果你看到这行注释，请确认文件后缀是 .ts
-// 然后就可以删掉本注释了!!!!!!!!!!!!!!!!
-
-
-
-
-
-
-
 const mdToJs = str => {
   const content = JSON.stringify(marked(str))
   return `export default ${content}`
