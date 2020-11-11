4a21441829377c4f6d07fcc8fcd59f1b84f35a60
xieyezi <1435398529@qq.com>
2020-11-11 12:56:51 +0800

    vite alias 设置还有bug

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vitePluginVuedoc from 'vite-plugin-vuedoc'
 const config: UserConfig = {
 	alias: {
 		// 键必须以斜线开始和结束
-		'/@vae-ui/': path.resolve(__dirname, './packages')
+		'/@vae-ui/': path.resolve(__dirname, 'packages')
 		// '/@components/': path.resolve(__dirname, './src/components')
 	},
 	plugins: [vitePluginVuedoc()]
